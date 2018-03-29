e71fd3b5daf30bef01fd566106977f3e28100d79
micklem95 <michaelvincentmoran@hotmail.com>
2018-03-29 14:24:46 +0100

    first step was to convert the string literal to a template literal so it would be easier to layout html. the next step was to add cards for the information that is being displayed on the webpage to be sat upon. the data was then set up so each user and their message is displayed on their own card, which was done by putting the owner on mat card title while the text was put into mat card content. body css was then added to the webpage to make the cards stand out a little more. the backgorund was given a ligfht grey tint to make the cards pop more significantly. in the module.ts component of the project, the rest of the material modules were imported from the angular installation that was done earlier. they also have to be included in the ngmodule import section

--- messageboard/frontend/src/app/app.module.ts
+++ messageboard/frontend/src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule,
+         MatCardModule, 
+         MatInputModule,
+         MatSnackBarModule,
+         MatToolbarModule } from '@angular/material';
 
 
 import { AppComponent } from './app.component';
@@ -16,7 +20,11 @@ import { MessagesComponent } from './messages.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- messageboard/frontend/src/app/messages.component.ts
+++ messageboard/frontend/src/app/messages.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'messages',
-    template: 'this is the messages component <div *ngFor = "let message of messages">{{message.text}} by: {{message.owner}}</div>'
+    template: 
+    `<div *ngFor = "let message of messages">
+        <mat-card style="margin:8px">
+        <mat-card-title>{{message.owner}}</mat-card-title>
+        <mat-card-content>{{message.text}}</mat-card-content>
+        </mat-card>
+    </div>
+    `
 })
 
 export class MessagesComponent {
