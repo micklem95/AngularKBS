bd120f0aca9c43091c8946d73312fb914b8eabd7
micklem95 <michaelvincentmoran@hotmail.com>
2018-04-05 14:04:24 +0100

    messages controller created, hard coded array written in for testing. Models folder created, inside a messages class was made. In this class, owner and messages are set.

---
+++ messageboard/MessageBoardBackend/MessageBoardBackend/Controllers/MessagesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessageBoardBackend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Messages")]
+    public class MessagesController : Controller
+    {
+        public IEnumerable <Models.Message> Get()
+        {
+            return new Models.Message[]
+            {
+                new Models.Message
+                {
+                    Owner = "John",
+                    Messages = "Hey"
+                },
+                new Models.Message
+                {
+                    Owner = "Bob",
+                    Messages = "Hi"
+                }
+
+            };
+        }
+    }
+}
