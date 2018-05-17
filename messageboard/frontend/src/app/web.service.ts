62258dd96b835bd7c27a0860973f2fc598066f72
micklem95 <michaelvincentmoran@hotmail.com>
2018-05-17 11:09:15 +0100

    try catch statements added to get and post functions in web service, error prompt visible to users and in chrome console

--- messageboard/frontend/src/app/web.service.ts
+++ messageboard/frontend/src/app/web.service.ts
@@ -1,6 +1,7 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/toPromise';
+import { MatSnackBar } from '@angular/material';
 
 @Injectable()
 export class WebService {
@@ -8,7 +9,7 @@ export class WebService {
 
     messages = [];
 
-    constructor(private http: Http) {
+    constructor(private http: Http, private sb : MatSnackBar) {
         this.getMessages();
     }
     async getMessages() {
@@ -16,12 +17,22 @@ export class WebService {
             var response = await this.http.get(this.BASE_URL +'/messages').toPromise();
             this.messages = response.json(); 
         } catch (error) {
-            console.error("Unable to get message");
+           this.handleError("Unable to get Messages");
         }
     }
 
     async postMessage(message){
-        var response = await this.http.post(this.BASE_URL + '/messages', message).toPromise();
-        this.messages.push(response.json())
+        try {
+            var response = await this.http.post(this.BASE_URL + '/messages', message).toPromise();
+            this.messages.push(response.json())
+        } catch (error) {
+            this.handleError("Unable to post Message");
+        }
+        
+    }
+
+    private handleError(error) {
+        console.error(error);
+        this.sb.open(error, 'Close', {duration: 2000});
     }
 }
